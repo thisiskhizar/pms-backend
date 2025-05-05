2af9bf3a2dcd765343e845e575b3f9cdffae4366
Khizar <khizar.ofc@gmail.com>
2025-05-05 21:09:51 +0500

    Register routes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContactViewSet, UnitViewSet, LeaseViewSet
+from .views import ContactViewSet, UnitViewSet, LeaseViewSet, dashboard_view, index_view
 
 router = DefaultRouter()
 router.register(r'contacts', ContactViewSet)
@@ -9,4 +9,6 @@ router.register(r'leases', LeaseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('index/', index_view, name='index'),
 ]
